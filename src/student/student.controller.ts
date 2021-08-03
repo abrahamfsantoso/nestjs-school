be33777db1094e4c1a6820006b2e79c9c476d943
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-08-03 15:19:18 +0700

    added request object

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('students')
 export class StudentController {
@@ -8,19 +8,23 @@ export class StudentController {
   }
 
   @Get('/:studentId')
-  getStudentById() {
-    // @Req() req: Request
-    // return req;
-    return 'Get Student By Id';
+  //    getStudentById(@Param() params:  {studentId: string}) {
+  //    console.log(params)
+  //    const {studentId} = params
+  getStudentById(@Param('studentId') studentId: string) {
+    return `Get Student Where Id is ${studentId}`;
   }
 
   @Post()
-  createStudent() {
-    return 'Create Student';
+  // createStudent(@Body('name') name) { // if only extract name
+  createStudent(@Body() body) {
+    return `Create Student With The Following Data ${JSON.stringify(body)}`;
   }
 
   @Put('/:studentId')
-  updateStudent() {
-    return 'Update Student By Id';
+  updateStudent(@Param('studentId') studentId: string, @Body() body) {
+    return `Update Student With Id of ${studentId} With Data of ${JSON.stringify(
+      body,
+    )}`;
   }
 }
