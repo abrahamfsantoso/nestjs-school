53f47b0027bf03ab24ab40ddf85eca7517c09cdf
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-08-03 19:37:34 +0700

    added provider

--- src/teacher/student.controller.ts
+++ src/teacher/student.controller.ts
@@ -3,14 +3,17 @@ import {
   FindStudentsResponseDto,
   StudentResponseDto,
 } from '../student/dto/student.dto';
+import { StudentService } from '../student/student.service';
 
 @Controller('teachers/:teacherId/students')
 export class StudentTeacherController {
+  constructor(private readonly studentService: StudentService) {}
+
   @Get()
   getStudents(
     @Param('teacherId') teacherId: string,
   ): FindStudentsResponseDto[] {
-    return `Get All Students That Belong To A Teacher With Id of ${teacherId}`;
+    return this.studentService.getStudentsByTeacherId(teacherId);
   }
 
   @Put('/:studentId')
@@ -18,6 +21,6 @@ export class StudentTeacherController {
     @Param('teacherId') teacherId: string,
     @Param('studentId') studentId: string,
   ): StudentResponseDto {
-    return `Update Student With Id Of ${studentId} To Teacher With Id Of ${teacherId}`;
+    return this.studentService.updateStudentTeacher(teacherId, studentId);
   }
 }
