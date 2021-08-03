be0dd7a4e0080a23ad70a941214f0712aced4ec8
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-08-03 20:05:07 +0700

    refactor module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
-import { StudentController } from '../student/student.controller';
-import { TeacherController } from '../teacher/teacher.controller';
-import { StudentTeacherController } from '../teacher/student.controller';
-import { StudentService } from '../student/student.service';
-import { TeacherService } from '../teacher/teacher.service';
+// import { StudentController } from '../student/student.controller';
+// import { TeacherController } from '../teacher/teacher.controller';
+// import { StudentTeacherController } from '../teacher/student.controller';
+// import { StudentService } from '../student/student.service';
+// import { TeacherService } from '../teacher/teacher.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [StudentController, TeacherController, StudentTeacherController],
+//   providers: [StudentService, TeacherService],
+// })
+// export class AppModule {}
+
+import { StudentsModule } from '../student/student.module';
+import { TeacherModule } from '../teacher/teacher.module';
 
 @Module({
-  imports: [],
-  controllers: [StudentController, TeacherController, StudentTeacherController],
-  providers: [StudentService, TeacherService],
+  imports: [StudentsModule, TeacherModule],
 })
 export class AppModule {}
